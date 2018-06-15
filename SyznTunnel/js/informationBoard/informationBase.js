3bfc0e32088192ce1aa8d3cac52527d40bbc2b1f
Kelly0427 <ML_0427@163.com>
2018-06-15 14:10:13 +0800

    remove directory from remote repository

--- SyznTunnel/js/informationBoard/informationBase.js
+++
@@ -1,8 +0,0 @@
-//全选
-checkAll("#checkAll","input[name='id[]']");
-//命令节目类型(search)
-commandType('#commandType');
-//情报板尺寸
-boardSizeTypeItem('#boardSizeSearch');
-var size = 10;
-informationBase(1,size);
